7001589a40f39126b7a7347dac40aa5417b87ec1
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2020-12-29 15:20:39 +0545

    Add files via upload

---
+++ binary search.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+#include <math.h>
+
+int main()
+{
+	int n;
+	
+	printf("how many elements?\n");
+	scanf("%d",&n);
+	
+	int arr[n], i, x, j, m;
+	
+	printf("enter %d element(s)\n",n);
+	
+	for(i = 0; i < n; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	i = 0;
+	j = n-1;
+	
+	printf("which elements to search?\n");
+	scanf("%d",&x);
+	
+	while(i < j)
+	{
+		m = floorf((i+j)/2);
+		
+		if(x > arr[m])
+		{
+			i = m + 1;
+		}
+		
+		else
+		{
+			j = m;
+		}
+		
+	}
+	
+	if(x = arr[i])
+	{
+		printf("%d is in %d place", x, i+1);
+	}
+	
+	else
+	{
+		printf("not found");
+	}
+	return 0;
+}
